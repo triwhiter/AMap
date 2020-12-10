317977a7a5334b3b5a9ac487de98f5f0e0e5a742
FlxLin <478237430@qq.com>
2020-12-10 16:07:37 +0800

    finish

--- app/src/androidTest/java/com/ctgu/linlinmap/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/ctgu/linlinmap/ExampleInstrumentedTest.java
@@ -9,11 +9,6 @@ import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
 
-/**
- * Instrumentation test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
     @Test
@@ -21,6 +16,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.tnboysmap", appContext.getPackageName());
+        assertEquals("com.ctgu.linlinmap", appContext.getPackageName());
     }
 }
